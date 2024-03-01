1225619802ea10be90760ea03c2e67d86a245a47
mohdfj <20230269@student.unifiji.ac.fj>
2024-03-01 15:19:32 +1200

    New Update v10

--- 20230269Mohd.py
+++ 20230269Mohd.py
@@ -1,1 +1,10 @@
-print("Welcome to the zone")
+def calAGE():
+    username = str(input("Please Enter Your Name: "))
+    age = int(input("Please Enter Your Age: "))
+
+    birth_year = 2024 - age
+
+    print("Hi,", username)
+    print("Your Birth Year is:", birth_year)
+
+calAGE()
